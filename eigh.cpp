eae0a436c4af5ce9335cec60a6e79620726d5e97
Lauri Niemi <lauri.niemi@csc.fi>
2025-02-11 18:41:57 +0200

    Refactoring, removing duplicate code

--- eigh.cpp
+++ eigh.cpp
@@ -11,20 +11,12 @@
 #include <complex>
 #include <type_traits>
 #include <iomanip>
+#include <concepts>
 
 #ifdef MAGMA
   #include "magma_v2.h"
   #include "magma_operators.h"
 #endif
-
-// Check if templated parameter is std::complex
-template<typename T>
-struct is_complex_t : public std::false_type {};
-
-template<typename T>
-struct is_complex_t<std::complex<T>> : public std::true_type {};
-//~
-
 
 #if defined(CUDA)
   #include <cuda_runtime.h>
@@ -56,6 +48,60 @@ struct is_complex_t<std::complex<T>> : public std::true_type {};
 #endif
 
 
+// Check if templated parameter is std::complex
+template<typename T>
+struct is_complex_t : public std::false_type {};
+
+template<typename T>
+struct is_complex_t<std::complex<T>> : public std::true_type {};
+
+template <typename T>
+concept Complex = is_complex_t<T>::value;
+
+template<typename T>
+concept Real = not is_complex_t<T>();
+//~
+
+// Helper type for sharing template code between complex and real calculations
+template<typename T>
+struct maybe_complex;
+
+template<Complex T>
+struct maybe_complex<T> {
+    using full_t = T;
+    using real_t = T::value_type;
+};
+
+
+template<Real T>
+struct maybe_complex<T> {
+    using full_t = T;
+    using real_t = T;
+};
+
+template <typename T>
+struct solver_backend_types;
+
+template <Real T>
+struct solver_backend_types<T> {
+    using dtype_eigval = T;
+    using dtype_matrix = T;    
+};
+
+template <Complex T>
+struct solver_backend_types<T> {
+    using dtype_eigval = typename T::value_type;
+    
+#if defined(MAGMA)
+    using dtype_matrix = std::conditional<std::is_same<dtype_eigval, float>::value, magmaFloatComplex, magmaDoubleComplex>::type;
+#elif defined(CUDA)
+    using dtype_matrix = std::conditional<std::is_same<dtype_eigval, float>::value, cuFloatComplex, cuDoubleComplex>::type;
+#elif defined(HIP)
+    using dtype_matrix = rocblas_complex_num<dtype_eigval>;
+#endif
+};
+
+
 #if defined(MAGMA)
   #define uplo_t           magma_uplo_t
   #define UPLO_LOWER       MagmaLower
@@ -64,28 +110,34 @@ struct is_complex_t<std::complex<T>> : public std::true_type {};
   #define VEC_MODE_NO      MagmaNoVec
   #define VEC_MODE_YES     MagmaVec
 
-  template <typename T>
-  magma_int_t (*magma_syevd_gpu)(magma_vec_t, magma_uplo_t, magma_int_t, T*, magma_int_t, T*, T*, magma_int_t, T*, magma_int_t, magma_int_t*, magma_int_t, magma_int_t*);
-
-  template<>
-  magma_int_t (*magma_syevd_gpu<float>)(magma_vec_t, magma_uplo_t, magma_int_t, float*, magma_int_t, float*, float*, magma_int_t, float*, magma_int_t, magma_int_t*, magma_int_t, magma_int_t*) = &magma_ssyevd_gpu;
-
-  template<>
-  magma_int_t (*magma_syevd_gpu<double>)(magma_vec_t, magma_uplo_t, magma_int_t, double*, magma_int_t, double*, double*, magma_int_t, double*, magma_int_t, magma_int_t*, magma_int_t, magma_int_t*) = &magma_dsyevd_gpu;
-
-  // Templated wrapper for MAGMA complex types
   template<typename T>
-  using magma_complex_num = typename std::conditional<std::is_same<T, float>::value, magmaFloatComplex, magmaDoubleComplex>::type;
+  struct MagmaHelpers {
 
-  // MAGMA complex solvers (HEEVD)
-  template<typename T>
-  magma_int_t (*magma_heevd_gpu)(magma_vec_t jobz, magma_uplo_t uplo, magma_int_t n, magma_complex_num<T> *dA, magma_int_t ldda, T *w, magma_complex_num<T> *wA, magma_int_t ldwa, magma_complex_num<T> *work, magma_int_t lwork, T *rwork, magma_int_t lrwork, magma_int_t *iwork, magma_int_t liwork, magma_int_t *info);
+      using matrix_dtype = typename solver_backend_types<T>::dtype_matrix;
+      using real_t = typename solver_backend_types<T>::dtype_eigval;
 
-  template<>
-  magma_int_t (*magma_heevd_gpu<float>)(magma_vec_t, magma_uplo_t, magma_int_t, magma_complex_num<float>*, magma_int_t, float*, magma_complex_num<float>*, magma_int_t, magma_complex_num<float>*, magma_int_t, float*, magma_int_t, magma_int_t*, magma_int_t, magma_int_t*) = &magma_cheevd_gpu;
-
-  template<>
-  magma_int_t (*magma_heevd_gpu<double>)(magma_vec_t, magma_uplo_t, magma_int_t, magma_complex_num<double>*, magma_int_t, double*, magma_complex_num<double>*, magma_int_t, magma_complex_num<double>*, magma_int_t, double*, magma_int_t, magma_int_t*, magma_int_t, magma_int_t*) = &magma_zheevd_gpu;
+      // Common eigensolver. For real types the rwork and lrwork inputs are ignored
+      static magma_int_t magma_eigsolver_gpu(magma_vec_t jobz, magma_uplo_t uplo, magma_int_t n, matrix_dtype *dA,
+        magma_int_t ldda, real_t *w, matrix_dtype *wA, magma_int_t ldwa, matrix_dtype *work, magma_int_t lwork,
+        real_t *rwork, magma_int_t lrwork, magma_int_t *iwork, magma_int_t liwork, magma_int_t *info) {
+            
+            if constexpr (std::is_same<T, std::complex<float>>::value) {
+                return magma_cheevd_gpu(jobz, uplo, n, dA, ldda, w, wA, ldwa, work, lwork, rwork, lrwork, iwork, liwork, info);
+            }
+            else if constexpr (std::is_same<T, std::complex<double>>::value) {
+                return magma_zheevd_gpu(jobz, uplo, n, dA, ldda, w, wA, ldwa, work, lwork, rwork, lrwork, iwork, liwork, info);
+            }
+            else if constexpr (std::is_same<T, float>::value) {
+                return magma_ssyevd_gpu(jobz, uplo, n, dA, ldda, w, wA, ldwa, work, lwork, iwork, liwork, info);
+            }
+            else if constexpr (std::is_same<T, double>::value) {
+                return magma_dsyevd_gpu(jobz, uplo, n, dA, ldda, w, wA, ldwa, work, lwork, iwork, liwork, info);
+            }
+            else {
+                static_assert(false, "magma_eigsolver_gpu not implemented for your template param");
+            }
+        }
+  };
 
 #elif defined(CUDA)
   #define uplo_t           cublasFillMode_t
@@ -119,44 +171,43 @@ struct is_complex_t<std::complex<T>> : public std::true_type {};
   #define VEC_MODE_NO      rocblas_evect_none
   #define VEC_MODE_YES     rocblas_evect_original
 
-  template <typename T>
-  rocblas_status (*rocsolver_syevd)(rocblas_handle, const rocblas_evect, const rocblas_fill, const rocblas_int, T*, const rocblas_int, T*, T*, rocblas_int*);
+  template<typename T>
+  struct RocHelpers {
 
-  template<>
-  rocblas_status (*rocsolver_syevd<float>)(rocblas_handle, const rocblas_evect, const rocblas_fill, const rocblas_int, float*, const rocblas_int, float*, float*, rocblas_int*) = &rocsolver_ssyevd;
+      using matrix_dtype = typename solver_backend_types<T>::dtype_matrix;
+      using real_t = typename solver_backend_types<T>::dtype_eigval;
 
-  template<>
-  rocblas_status (*rocsolver_syevd<double>)(rocblas_handle, const rocblas_evect, const rocblas_fill, const rocblas_int, double*, const rocblas_int, double*, double*, rocblas_int*) = &rocsolver_dsyevd;
-
-  // Eigensolvers for complex diagonalization (Hermitian matrices, eigenvalues are real)
-
-  template <typename real_t>
-  rocblas_status (*rocsolver_heevd)(rocblas_handle, const rocblas_evect, const rocblas_fill, const rocblas_int, rocblas_complex_num<real_t>*, const rocblas_int, real_t*, real_t*, rocblas_int*);
-
-  // rocblas_float_complex
-  template<>
-  rocblas_status (*rocsolver_heevd<float>)(rocblas_handle, const rocblas_evect, const rocblas_fill, const rocblas_int, rocblas_complex_num<float>*, const rocblas_int, float*, float*, rocblas_int*) = &rocsolver_cheevd;
-
-  // rocblas_double_complex
-  template<>
-  rocblas_status (*rocsolver_heevd<double>)(rocblas_handle, const rocblas_evect, const rocblas_fill, const rocblas_int, rocblas_complex_num<double>*, const rocblas_int, double*, double*, rocblas_int*) = &rocsolver_zheevd;
+      // Common eigensolver
+      static rocblas_status roc_common_eigsolver(
+        rocblas_handle handle,
+        const rocblas_evect evect,
+        const rocblas_fill uplo,
+        const rocblas_int n,
+        matrix_dtype* dA,
+        const rocblas_int lda,
+        real_t* D,
+        real_t* E,
+        rocblas_int* info) {
+            
+            if constexpr (std::is_same<T, std::complex<float>>::value) {
+                return rocsolver_cheevd(handle, evect, uplo, n, dA, lda, D, E, info);
+            }
+            else if constexpr (std::is_same<T, std::complex<double>>::value) {
+                return rocsolver_zheevd(handle, evect, uplo, n, dA, lda, D, E, info);
+            }
+            else if constexpr (std::is_same<T, float>::value) {
+                return rocsolver_ssyevd(handle, evect, uplo, n, dA, lda, D, E, info);
+            }
+            else if constexpr (std::is_same<T, double>::value) {
+                return rocsolver_dsyevd(handle, evect, uplo, n, dA, lda, D, E, info);
+            }
+            else {
+                static_assert(false, "magma_eigsolver_gpu not implemented for your template param");
+            }
+        }
+  };
 
 #endif // ~HIP
-
-
-template <typename T>
-struct backend_complex
-{
-    using real_t = T;
-#if defined(MAGMA)
-    using type = magma_complex_num<T>;
-#elif defined(CUDA)
-    using type = typename std::conditional<std::is_same<float, T>::value, cuFloatComplex, cuDoubleComplex>::type;
-#elif defined(HIP)
-    using type = rocblas_complex_num<T>;
-#endif
-};
-
 
 constexpr int N_MAX_PRINT = 3;
 
@@ -190,8 +241,8 @@ void print_matrix(const int &n, const std::vector<T> &A) {
 
 
 template<typename T>
-std::vector<T> build_hermitian_matrix(uint32_t seed, uint32_t matrix_size)
-{
+std::vector<T> build_hermitian_matrix(uint32_t seed, uint32_t matrix_size) {
+
     static_assert(is_complex_t<T>::value, "build_hermitian_matrix<T>() needs std::complex -valued T");
 
     using real_t = typename T::value_type;
@@ -225,8 +276,8 @@ std::vector<T> build_hermitian_matrix(uint32_t seed, uint32_t matrix_size)
 
 // Returns a random symmetric matrix
 template<typename T>
-std::vector<T> build_test_matrix(uint32_t seed, uint32_t matrix_size)
-{
+std::vector<T> build_test_matrix(uint32_t seed, uint32_t matrix_size) {
+
     std::mt19937 gen(seed);
     std::uniform_real_distribution<T> dis(0.0, 1.0);
     const uint32_t n = matrix_size;
@@ -251,19 +302,17 @@ std::vector<T> build_test_matrix(uint32_t seed, uint32_t matrix_size)
 // Specializations for complex numbers, these return a random Hermitian matrix
 
 template<>
-std::vector<std::complex<float>> build_test_matrix(uint32_t seed, uint32_t matrix_size)
-{    
+std::vector<std::complex<float>> build_test_matrix(uint32_t seed, uint32_t matrix_size) {    
     return build_hermitian_matrix<std::complex<float>>(seed, matrix_size);
 }
 
 template<>
-std::vector<std::complex<double>> build_test_matrix(uint32_t seed, uint32_t matrix_size)
-{    
+std::vector<std::complex<double>> build_test_matrix(uint32_t seed, uint32_t matrix_size) {    
     return build_hermitian_matrix<std::complex<double>>(seed, matrix_size);
 }
 
-// T is real valued (float/double), use the bool argument to choose between real vs complex calculation
-template<typename T, bool bComplex>
+
+template<typename T>
 struct Calculator {
     cudaStream_t stream;
     int h_info;
@@ -272,32 +321,33 @@ struct Calculator {
     uplo_t uplo;
     vec_mode_t vec;
 
-    static_assert(!is_complex_t<T>(), "Calculator<T, bComplex> needs real-valued T");
+    using eigval_t = typename maybe_complex<T>::real_t;
 
-    using h_complex = typename std::complex<T>;
-    using d_complex = typename backend_complex<T>::type;
+    using backend_dtype = typename solver_backend_types<T>::dtype_matrix;
+    using backend_eigval_t = typename solver_backend_types<T>::dtype_eigval;
+
+    static_assert(sizeof(backend_dtype) == sizeof(T), "Size mismatch in input matrix datatype vs backend matrix datatype");
+    static_assert(sizeof(backend_eigval_t) == sizeof(eigval_t), "Size mismatch in input real type vs backend real type");
 
 #if defined(MAGMA)
     magma_queue_t queue;
-    // magma work arrays need to be magma complex type, but note that these are actually alloc'd on the host...
-    d_complex *h_wA;
-    d_complex *h_work;
+    backend_dtype *h_wA;
+    backend_dtype *h_work;
     magma_int_t lwork;
     magma_int_t *h_iwork;
     magma_int_t liwork;
 
     // rwork and lrwork needed for complex MAGMA solver, not used by the real version
-    std::vector<T> rwork;
+    std::vector<backend_eigval_t> rwork;
     magma_int_t lrwork;
 
     // Find optimal workgroup sizes
-    void magma_query_work_sizes(magma_int_t &lwork_opt, magma_int_t &lrwork_opt, magma_int_t &liwork_opt)
-    {
-        d_complex work_temp;
-        T rwork_temp;
+    void magma_query_work_sizes(magma_int_t &lwork_opt, magma_int_t &lrwork_opt, magma_int_t &liwork_opt) {
+        backend_dtype work_temp;
+        backend_eigval_t rwork_temp;
         magma_int_t iwork_temp;
         
-        magma_heevd_gpu<T>(vec, uplo, n, nullptr, lda, nullptr, nullptr, lda, &work_temp, -1, &rwork_temp, -1, &iwork_temp, -1, &h_info);
+        MagmaHelpers<T>::magma_eigsolver_gpu(vec, uplo, n, nullptr, lda, nullptr, nullptr, lda, &work_temp, -1, &rwork_temp, -1, &iwork_temp, -1, &h_info);
 
         lwork_opt = static_cast<magma_int_t>(real(work_temp));
         lrwork_opt = static_cast<magma_int_t>(rwork_temp);
@@ -318,7 +368,7 @@ struct Calculator {
 #else
     rocblas_handle handle;
     int *d_info;
-    T *d_work;
+    backend_eigval_t *d_work;
 
 #endif
 
@@ -336,8 +386,8 @@ struct Calculator {
         magma_query_work_sizes(lwork, lrwork, liwork);
 
         // Allocate work arrays
-        h_wA = reinterpret_cast<d_complex*>(malloc(sizeof(d_complex) * lda*n));
-        h_work = reinterpret_cast<d_complex*>(malloc(sizeof(d_complex) * lwork));
+        h_wA = reinterpret_cast<backend_dtype*>(malloc(sizeof(backend_dtype) * lda*n));
+        h_work = reinterpret_cast<backend_dtype*>(malloc(sizeof(backend_dtype) * lwork));
         h_iwork = reinterpret_cast<magma_int_t*>(malloc(sizeof(magma_int_t) * liwork));
 
 #elif defined(CUDA)
@@ -401,20 +451,20 @@ struct Calculator {
 
     // Solve eigensystem. Eigenvectors will be optionally copied to h_V if not null.
     void calculate(
-        const d_complex* d_A_input,
-        T* d_W,
-        T* h_W,
-        h_complex* h_V = nullptr) {
+        const backend_dtype* d_A_input,
+        backend_eigval_t* d_W,
+        eigval_t* h_W,
+        T* h_V = nullptr) {
 
         // The input array gets overwritten so we work on a copy
-        d_complex *d_A;
-        cudaMalloc(reinterpret_cast<void **>(&d_A), sizeof(d_complex) * lda*n);
-        cudaMemcpyAsync(d_A, d_A_input, sizeof(d_complex) * lda*n, cudaMemcpyDeviceToDevice, stream);
+        backend_dtype *d_A;
+        cudaMalloc(reinterpret_cast<void **>(&d_A), sizeof(backend_dtype) * lda*n);
+        cudaMemcpyAsync(d_A, d_A_input, sizeof(backend_dtype) * lda*n, cudaMemcpyDeviceToDevice, stream);
 
 #if defined(MAGMA)
 
         rwork.resize(lrwork);
-        magma_heevd_gpu<T>(vec, uplo, n, d_A, lda, h_W, h_wA, lda, h_work, lwork, rwork.data(), lrwork, h_iwork, liwork, &h_info);
+        MagmaHelpers<T>::magma_eigsolver_gpu(vec, uplo, n, d_A, lda, h_W, h_wA, lda, h_work, lwork, rwork.data(), lrwork, h_iwork, liwork, &h_info);
 
         // Copy eigenvectors to GPU
         cudaMemcpyAsync(d_W, h_W, sizeof(T) * n, cudaMemcpyHostToDevice, stream);
@@ -428,7 +478,7 @@ struct Calculator {
         cudaStreamSynchronize(stream);
 
 #elif defined(HIP)
-        rocsolver_heevd<T>(handle, vec, uplo, n, d_A, lda, d_W, d_work, d_info);
+        RocHelpers<T>::roc_common_eigsolver(handle, vec, uplo, n, d_A, lda, d_W, d_work, d_info);
         cudaMemcpyAsync(&h_info, d_info, sizeof(int), cudaMemcpyDeviceToHost, stream);
         cudaStreamSynchronize(stream);
 
@@ -450,46 +500,21 @@ struct Calculator {
 #endif
         // Eigenvectors are now in d_A
         if (h_V) {
-            cudaMemcpyAsync(h_V, d_A, sizeof(d_complex) * lda*n, cudaMemcpyDeviceToHost, stream);
+            cudaMemcpyAsync(h_V, d_A, sizeof(backend_dtype) * lda*n, cudaMemcpyDeviceToHost, stream);
             cudaStreamSynchronize(stream);
         }
         cudaFree(d_A);
     }
 };
 
-// Specialization for real numbers defined in a separate header
-#include "Calculator_real.hpp"
-
-
-// Helper type for sharing template code between complex and real calculations
-template<typename T>
-struct maybe_complex
-{
-    using complex = T;
-    using real_t = T;
-};
-
-template<typename U>
-struct maybe_complex<std::complex<U>>
-{
-    using real_t = U;
-
-#if defined(MAGMA)
-    using complex = magma_complex_num<U>;
-#elif defined(CUDA)
-    using complex = typename backend_complex<U>::type;
-#elif defined(HIP)
-    using complex = rocblas_complex_num<U>;
-#endif
-};
-
 template <typename T>
 void run(int n, int repeat) {
 
-    using real_t = typename maybe_complex<T>::real_t;
+    using eigval_t = typename maybe_complex<T>::real_t;
 
-    using d_full_type = typename maybe_complex<T>::complex;
-    using d_real_type = typename maybe_complex<T>::real_t;
+    using backend_dtype = typename solver_backend_types<T>::dtype_matrix;
+    using backend_eigval_t = typename solver_backend_types<T>::dtype_eigval;
+
 
     std::cout << "RUN"
               << " n: " << n
@@ -502,7 +527,7 @@ void run(int n, int repeat) {
     // Host eigenvectors, can be complex. Optionally copied from the device after finding solution
     std::vector<T> h_V(lda * n, 0);
     // Host eigenvalues, real
-    std::vector<real_t> h_W(n, 0);
+    std::vector<eigval_t> h_W(n, 0);
 
     // Build a test matrix. Will be symmetric for real T and Hermitian for complex T
     std::vector<T> h_A = build_test_matrix<T>(n, n);
@@ -511,12 +536,12 @@ void run(int n, int repeat) {
     print_matrix(n, h_A);
 
     // Device matrix (can be complex)
-    d_full_type *d_A = nullptr;
+    backend_dtype *d_A = nullptr;
     // Device eigenvalues (real)
-    d_real_type *d_W = nullptr;
+    backend_eigval_t *d_W = nullptr;
 
     cudaMalloc(reinterpret_cast<void **>(&d_A), sizeof(T) * h_A.size());
-    cudaMalloc(reinterpret_cast<void **>(&d_W), sizeof(d_real_type) * h_W.size());
+    cudaMalloc(reinterpret_cast<void **>(&d_W), sizeof(backend_eigval_t) * h_W.size());
     cudaMemcpy(d_A, h_A.data(), sizeof(T) * h_A.size(), cudaMemcpyHostToDevice);
     cudaDeviceSynchronize();
 
@@ -524,7 +549,7 @@ void run(int n, int repeat) {
     vec_mode_t vec = VEC_MODE_YES;
 
     {
-        Calculator<real_t, is_complex_t<T>::value> calc(n, uplo, vec);
+        Calculator<T> calc(n, uplo, vec);
 
         // Warm up
         calc.calculate(d_A, d_W, h_W.data(), h_V.data());
@@ -548,7 +573,7 @@ void run(int n, int repeat) {
         // Run timing recreating handles etc every time
         auto t0 = std::chrono::high_resolution_clock::now();
         for (int iter = 0; iter < repeat; iter++) {
-            Calculator<real_t, is_complex_t<T>::value> calc(n, uplo, vec);
+            Calculator<T> calc(n, uplo, vec);
             calc.calculate(d_A, d_W, h_W.data());
         }
         auto t1 = std::chrono::high_resolution_clock::now();
